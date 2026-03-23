917be9ea0b26b884c839aa24b5bb990bbb86335f
RBS <charleshu91@gmail.com>
2026-03-23 14:41:04 +0100

    feat
    - Add separate relative path and filename
    - Add separete and datetime sytle

--- rapid_full_file_and_relative_path_list_gen.py
+++ rapid_full_file_and_relative_path_list_gen.py
@@ -3,8 +3,6 @@ import os
 from datetime import datetime
 
 
-with zipfile.ZipFile('file.zip', 'r') as zip_ref:
+with zipfile.ZipFile('bck_2025_08_22.zip', 'r') as zip_ref:
     for info in zip_ref.infolist():
-        print(f"{info.filename}; {info.file_size}; {info.is_dir()}; ")
-
-
+        print(f"{info.filename}; {os.path.basename(info.filename)}; {os.path.dirname(info.filename)}/; {info.file_size}; {info.is_dir()}; {datetime(*info.date_time).strftime('%Y-%m-%d %H:%M:%S')}")
